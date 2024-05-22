8a52262a59c6e1cbf2ed63e445e2c0d5f0aa3d3e
viamazoba <viamazoba@unal.edu.co>
2024-05-22 01:01:58 -0500

    feat: Se añaden peticiones post, put y delete al proyecto.

--- users.py
+++ users.py
@@ -44,3 +44,44 @@ async def user(id: int):
 @app.get("/userquery/")
 async def user_query(id: int):
     return search_user(id)
+
+
+@app.post("/user/")
+async def user(user: User):
+    userResult = list(
+        filter(lambda userList: userList.id == user.id, users_list))
+    if userResult:
+        raise HTTPException(status_code=404, detail="User already exist")
+    else:
+        users_list.append(user)
+        raise HTTPException(status_code=200, detail="User added succesfully!")
+
+
+@app.put("/user/")
+async def inser_user(user: User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+
+    if found:
+        raise HTTPException(
+            status_code=200, detail="User updated succesfully!")
+    else:
+        raise HTTPException(status_code=404, detail="Error user not found")
+
+
+@app.delete("/user/{id}")
+async def delete_user(id: int):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+
+    if found:
+        raise HTTPException(
+            status_code=200, detail="User deleted succesfully!")
+    else:
+        raise HTTPException(status_code=404, detail="Error user not found")
